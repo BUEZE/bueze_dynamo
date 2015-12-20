b540d243f9888da84db87a6d45c677f183193c7b
crazyplum <crazyplum24@gmail.com>
2015-12-21 02:33:43 +0800

    modify model/bookranking

--- models/bookranking.rb
+++ models/bookranking.rb
@@ -1,7 +1,22 @@
-require 'sinatra'
-require 'sinatra/activerecord'
-require_relative '../config/environments.rb'
+require 'dynamoid'
 
-# Bookranking
-class Bookranking < ActiveRecord::Base
+class BookRanking
+  include Dynamoid::Document
+  field :booknames, :string
+  field :rank, :integer
+  field :price_description, :string
+  field :price, :integer
+  field :author, :string
+  field :date, :date
+  field :source, :string
+  field :prod_id, :string
+
+  def self.destroy(id)
+    find(id).destroy
+  end
+
+  def self.delete_all
+    all.each(&:delete)
+  end
 end
+  
